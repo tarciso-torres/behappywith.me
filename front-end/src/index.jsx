034a8dd0fbee0a5ee90f0bf53d38c27fd84bda6b
Tarciso Torres <tarcisoftorres@gmail.com>
2018-05-12 13:02:35 -0300

    First Commit

---
+++ front-end/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './img/favicon.ico';
+
+ReactDom.render(
+    <h1>Bem-vindo ao react!</h1>,
+    document.querySelector('#main')
+)
---
+++ front-end/webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.jsx'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: [".js",".jsx"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template:path.join(__dirname, 'src/index.html')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'src'),
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015', 'react']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|ico|png|gif|svg)$/i,
+                loader: 'file-loader?name=img/[name].[ext]'
+            }
+        ]
+    },
+    devServer: {
+        publicPath: "/",
+        contentBase: "./dist"
+    }
+};
