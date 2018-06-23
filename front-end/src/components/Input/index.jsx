d7d38866598c9003898afcce582d6e70bffb5e70
Tarciso Torres <tarcisoftorres@gmail.com>
2018-06-23 08:41:45 -0300

    Second version component input

---
+++ front-end/src/components/Input/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Input(props) {
+    const estilo = {
+        borderColor: props.valorInvalido ? '#d50000' : '#cccccc',
+        backgroundColor: props.valorInvalido ? 'ffcdd2' : '#ffffff'
+    };
+
+    return (
+        <input 
+           type="text"
+           style={estilo    }
+           {...props}
+        />   
+    )
+}
